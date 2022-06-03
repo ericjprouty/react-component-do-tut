de267846c70a892098c781c2120d0b64a15e3a20
Eric Prouty <ericjprouty@192.168.1.6>
2022-06-02 20:32:53 -0700

    Initial commit

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Button extends Component {
+
+// const displayButtonMessage = event => alert("I am an alert box!");;
+
+  render() {
+    return(
+        <button class="button-component" type="button">Click Here</button>
+    )
+  }
+
+}
+
+export default Button;
